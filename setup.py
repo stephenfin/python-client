6af2e2e5c8377da0f54f6b93c14977a8be497210
Stephen Finucane <stephen@that.guru>
2018-04-24 22:41:16 +0100

    setup: Remove get_packages, get_package_data
    The former of these duplicates functionality already provided by
    setuptools, while the latter doesn't do anything. Replace the first and
    remove the second.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import find_packages, setup
 import re
 import os
-import shutil
 import sys
 
 
@@ -14,31 +13,6 @@ def get_version(package):
     """
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
 
 
 version = get_version('coreapi')
@@ -60,8 +34,8 @@ setup(
     description='Python client library for Core API.',
     author='Tom Christie',
     author_email='tom@tomchristie.com',
-    packages=get_packages('coreapi'),
-    package_data=get_package_data('coreapi'),
+    packages=find_packages('coreapi'),
+    include_package_data=True,
     install_requires=[
         'coreschema',
         'requests',
